b8c59410320b813e8e6ba5085c61da0f5798add0
reykboerner <r.borner@uu.nl>
2026-08-25 15:20:53 +0200

    add streamfunction sigma coords

--- oceanalysis/diagnostics/__init__.py
+++ oceanalysis/diagnostics/__init__.py
@@ -1,6 +1,7 @@
 from ..model import OceanModel
 
-from .streamfunction import moc_streamfunction_depth, amoc_strength
+from .streamfunction import amoc_strength, moc_streamfunction_depth, moc_streamfunction_sigma
+
 
 class OceanDiagnostics:
 
@@ -10,20 +11,19 @@ class OceanDiagnostics:
         self.model = model
         self.grid = model.grid
 
-        # Reference values         
-        self.S0 = S0               # Reference salinity (g kg^-1)
-        self.T0 = T0               # Reference temperature (deg C)
-
+        # Reference values
+        self.S0 = S0  # Reference salinity (g kg^-1)
+        self.T0 = T0  # Reference temperature (deg C)
 
     def moc_streamfunction_depth(self, data, mask):
         return moc_streamfunction_depth(self.model, data, mask)
 
-    def moc_streamfunction_sigma(self, data, mask, sigmas):
-        pass
+    def moc_streamfunction_sigma(self, data, mask, sigmas, p_level=0):
+        return moc_streamfunction_sigma(self.model, data, mask, sigmas=sigmas, p_level=p_level)
 
     def amoc_strength(self, data, latitude=26, depth=1000):
         return amoc_strength(self.model, data, latitude, depth)
-    
+
     def surface_buoyancy_flux(self, data, mask):
         pass
 
